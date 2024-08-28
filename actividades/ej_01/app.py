c90f761a561c8add71458e42c0f72cc2b43798f4
ValentinoGonzalezPautaso2 <gonzalezpautasovalentino@gmail.com>
2024-08-28 16:24:46 -0300

    feat: Actividad N°1 Finalizada

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,14 @@
-# Aplicacion del servidor
+import microdot
+from microdot import send_file
+
+app = microdot.Microdot()
+
+@app.route('/')
+def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+def styles(request,dir,file):
+    return send_file("/"+dir+"/"+file)
+
+app.run(port=80)
